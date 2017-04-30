5c7b0d16624d8fbaacc0d4e5013970e5aa2740e2
Robin Bühler <public@openscript.ch>
2017-04-30 16:37:10 +0200

    Update a query

--- src/queries/all-employees-which-are-guests.sql
+++ src/queries/all-employees-which-are-guests.sql
@@ -4,5 +4,5 @@ SELECT p.forename AS "Vorname", p.surname AS "Nachname" FROM person AS p
   INNER JOIN booking_person AS g ON p.id = g.person_id;
 
 SELECT p.forename AS "Vorname", p.surname AS "Nachname" FROM person AS p
-  INNER JOIN booking_person AS g ON p.id = g.person_id
+  INNER JOIN booking AS b ON p.id = b.responsible_person_id
   INNER JOIN employee AS e ON p.id = e.person_id;
