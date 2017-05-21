cca5e7fc4250c9adfe164b1c49ef4946bd5d9803
Robin Bühler <public@openscript.ch>
2017-05-21 12:17:41 +0200

    Add default connection button

--- src/programs/4-buchungen-anzeigen/src/main/java/ch/ffhs/starview/bookings/ConnectionPane.java
+++ src/programs/4-buchungen-anzeigen/src/main/java/ch/ffhs/starview/bookings/ConnectionPane.java
@@ -1,4 +1,35 @@
 package ch.ffhs.starview.bookings;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionPane {
+    @FXML
+    public TextField connectionTextField;
+
+    @FXML
+    public void handleConnectAction(ActionEvent actionEvent) {
+        try {
+            Connection connection = DriverManager.getConnection(connectionTextField.getText());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    public void handleDefaultServerAction(ActionEvent actionEvent) {
+        connectionTextField.setText("jdbc:mysql://86.119.36.10:41337/starview_robin?" +
+                "user=robin&" +
+                "password=ejNT3~K:8x*N&" +
+                "useSSL=false&" +
+                "useUnicode=true&" +
+                "useJDBCCompliantTimezoneShift=true&" +
+                "useLegacyDatetimeCode=false&" +
+                "serverTimezone=UTC");
+    }
 }
